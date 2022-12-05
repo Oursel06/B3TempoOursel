2f4a523d947e3de0e97f312bde269a96632b6f94
Oursel06 <alexoursel6@gmail.com>
2022-12-05 10:40:45 +0100

    Http vers getParamNBJ

---
+++ app/src/main/java/com/example/b3tempooursel/IEdfApi.java
@@ -0,0 +1,16 @@
+package com.example.b3tempooursel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IEdfApi {
+
+    String EDF_TEMPO_API_ALERT_TYPE = "TEMPO";
+
+    // https://particulier.edf.fr/services/rest/referentiel/getNbTempoDays?TypeAlerte=TEMPO
+
+    @GET("services/rest/referentiel/getNbTempoDays")
+    Call<TempoDaysLeft> getTempoDaysLeft(@Query("TypeAlerte") String alertType);
+
+}
--- app/src/main/java/com/example/b3tempooursel/MainActivity.java
+++ app/src/main/java/com/example/b3tempooursel/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.b3tempooursel;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.net.HttpURLConnection;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView responseText;
+    IEdfApi apiInterface;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        responseText = (TextView) findViewById(R.id.response_text);
+        apiInterface = ApiClient.get().create(IEdfApi.class);
+
+        Call<TempoDaysLeft> call = apiInterface.getTempoDaysLeft(IEdfApi.EDF_TEMPO_API_ALERT_TYPE);
+        call.enqueue(new Callback<TempoDaysLeft>() {
+            @Override
+            public void onResponse(Call<TempoDaysLeft> call, Response<TempoDaysLeft> response) {
+                TempoDaysLeft resource = response.body();
+                if(response.code() == HttpURLConnection.HTTP_OK && resource != null) {
+                    responseText.setText(resource.getParamNbJBlanc().toString());
+                    Log.d("REPONSE", "getParamNbJBleu : " + resource.getParamNbJBleu());
+                    Log.d("REPONSE", "getParamNbJBlanc : " + resource.getParamNbJBlanc());
+                    Log.d("REPONSE", "getParamNbJRouge : " + resource.getParamNbJRouge());
+                }
+            }
+            @Override
+            public void onFailure(Call<TempoDaysLeft> call, Throwable t) {
+                call.cancel();
+            }
+        });
     }
 }
