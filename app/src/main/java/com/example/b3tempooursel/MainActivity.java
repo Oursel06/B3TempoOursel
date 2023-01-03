23b777e8f85c0d824677eea6e99321e5ec307be9
Oursel06 <alexoursel6@gmail.com>
2023-01-03 11:42:40 +0100

    Ajout config notif

--- app/src/main/java/com/example/b3tempooursel/MainActivity.java
+++ app/src/main/java/com/example/b3tempooursel/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.b3tempooursel;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.net.HttpURLConnection;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView red_days_tv, blue_days_tv, white_days_tv;
+    public static final String CHANNEL_ID = "tempo_alert_channel_id";
     public static IEdfApi apiInterface = ApiClient.get().create(IEdfApi.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,37 +27,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button history = findViewById(R.id.history_bt);
         history.setOnClickListener(this);
         getDayLeft();
+        createNotificationChannel();
     }
 
     @Override
     public void onClick(View v) {
-            Intent intent = new Intent();
-            intent.setClass(this, HistoryActivity.class);
-            startActivity(intent);
-        }
+        Intent intent = new Intent();
+        intent.setClass(this, HistoryActivity.class);
+        startActivity(intent);
+    }
 
-        public void getDayLeft(){
-            Call<TempoDaysLeft> call = apiInterface.getTempoDaysLeft(IEdfApi.EDF_TEMPO_API_ALERT_TYPE);
-            call.enqueue(new Callback<TempoDaysLeft>() {
-                @Override
-                public void onResponse(Call<TempoDaysLeft> call, Response<TempoDaysLeft> response) {
-                    TextView blue_days_tv_txt = findViewById(R.id.blue_days_tv);
-                    TextView white_days_tv_txt = findViewById(R.id.white_days_tv);
-                    TextView red_days_tv_txt = findViewById(R.id.red_days_tv);
-                    TempoDaysLeft resource = response.body();
-                    if(response.code() == HttpURLConnection.HTTP_OK && resource != null) {
-                        Log.d("REPONSE", "getParamNbJBleu : " + resource.getParamNbJBleu());
-                        Log.d("REPONSE", "getParamNbJBlanc : " + resource.getParamNbJBlanc());
-                        Log.d("REPONSE", "getParamNbJRouge : " + resource.getParamNbJRouge());
-                        blue_days_tv_txt.setText(resource.getParamNbJBleu().toString());
-                        white_days_tv_txt.setText(resource.getParamNbJBlanc().toString());
-                        red_days_tv_txt.setText(resource.getParamNbJRouge().toString());
-                    }
+    public void getDayLeft() {
+        Call<TempoDaysLeft> call = apiInterface.getTempoDaysLeft(IEdfApi.EDF_TEMPO_API_ALERT_TYPE);
+        call.enqueue(new Callback<TempoDaysLeft>() {
+            @Override
+            public void onResponse(Call<TempoDaysLeft> call, Response<TempoDaysLeft> response) {
+                TextView blue_days_tv_txt = findViewById(R.id.blue_days_tv);
+                TextView white_days_tv_txt = findViewById(R.id.white_days_tv);
+                TextView red_days_tv_txt = findViewById(R.id.red_days_tv);
+                TempoDaysLeft resource = response.body();
+                if (response.code() == HttpURLConnection.HTTP_OK && resource != null) {
+                    Log.d("REPONSE", "getParamNbJBleu : " + resource.getParamNbJBleu());
+                    Log.d("REPONSE", "getParamNbJBlanc : " + resource.getParamNbJBlanc());
+                    Log.d("REPONSE", "getParamNbJRouge : " + resource.getParamNbJRouge());
+                    blue_days_tv_txt.setText(resource.getParamNbJBleu().toString());
+                    white_days_tv_txt.setText(resource.getParamNbJBlanc().toString());
+                    red_days_tv_txt.setText(resource.getParamNbJRouge().toString());
                 }
-                @Override
-                public void onFailure(Call<TempoDaysLeft> call, Throwable t) {
-                    call.cancel();
-                }
-            });
+            }
+
+            @Override
+            public void onFailure(Call<TempoDaysLeft> call, Throwable t) {
+                call.cancel();
+            }
+        });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
+    private void checkColorForNotif(TempoColor color){
+        if(color == TempoColor.RED || color == TempoColor.WHITE){
+
+        }
+    }
+}
